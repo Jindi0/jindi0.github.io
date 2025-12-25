328b861676eae62186beed683caa16148641defa
Jindi0 <66841061+Jindi0@users.noreply.github.com>
2025-12-25 21:20:15 +0000

    Deploying to gh-pages from @ Jindi0/jindi0.github.io@89b3bf747f3267c872c68969421a049e3a1b0490 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -479,7 +479,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=https://scholar.google.com/citations?user=LVHh5PIAAAAJ&hl=en", "_blank");
+          window.open("https://scholar.google.com/citations?user=LVHh5PIAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
